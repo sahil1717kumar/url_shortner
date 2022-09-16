9bdb2cdc5e71961f462a2d3511bb3a32e69b6010
shail kumar <sahil1717kumar@gmail.com>
2022-09-17 03:58:04 +0530

    for heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 dotenv.config()
-const port =3000
+const port = process.env.PORT || 3000
 const app = express()
 const initConnection = require("./database/init")
 const entities = require("./entities")
